152ca5deeae91afca1ee834cc3e5397ed9f65bfa
Miron Lončarić <miron.loncaric@gmail.com>
2020-04-14 21:04:01 +0200

    Dodan je kod za hvatanje pogrešaka

--- Funkcije/Zadatak1.py
+++ Funkcije/Zadatak1.py
@@ -3,8 +3,15 @@ def definition(num) :
     for i in range(1, 11) :
         print("{} * {} = {}".format(num, i, num*i))
 
-# unosimo broj i pohranjujemo ga u varjablu num
-num = int(input("--> "))
+# bilo mi je dosadno pa sam dodao error handling
+while True :
+    # program pokušava unesti cijeli broj te izlazi iz petlje ukoliko uspije
+    try :
+        num = int(input("--> "))
+        break
+    # ukoliko program naiđe na pogrešku tipa ValueError ispisuje poruku i vrača se na početak petlje
+    except ValueError :
+        print("Molim vas unesite cijeli broj --> ")
 
-# zovemo funkciju definition() i unosimo broj num kao parametar 
+# zovemo funkciju definition() i unosimo cijeli broj num kao parametar 
 definition(num)
