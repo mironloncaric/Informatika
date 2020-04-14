b0ea196fa31dab5336b076c33ba4036a549b3574
Miron Lončarić <miron.loncaric@gmail.com>
2020-04-14 21:14:28 +0200

    Dodani su komentari radi čitljivosti koda.

--- Zadatci/zadatak4.py
+++ Zadatci/zadatak4.py
@@ -1,4 +1,6 @@
 for i in range(1, 4) :
     for j in range (1, 4) :
+        # provjerava se je li broj i jednak broju j
+        # ukoliko se brojevi razlikuju, ispisuju se
         if (i != j) :
             print("({}, {})".format(i, j))
